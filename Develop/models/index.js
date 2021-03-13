b40c18c3b6f2409369f7f741ac8934804fb4fa16
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-13 17:53:19 -0500

    defined model relationships in model/index

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: {
+    model:ProductTag,
+    unique: false,
+  },
+  as: 'product_refs'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tag_refs'
+})
 
 module.exports = {
   Product,
