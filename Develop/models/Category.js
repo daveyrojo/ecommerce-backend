6c3e8857a460d1cc6612032ab01bfa5889bbc32a
David Eldridge <d.vincenteldridge@gmail.com>
2021-03-13 17:29:56 -0500

    made category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,13 +7,32 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    products: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+        unqiue: false
+      }
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
